81afedaa86f75ee9a42ef2b1c8abc9d648baa276
luis <lcrodriguez@uamv.edu.ni>
2026-05-29 12:05:40 -0600

    agregue la funcion agregar empleados

--- RegistroEmpleadp/Program.cs
+++ RegistroEmpleadp/Program.cs
@@ -1,1 +1,28 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Empleado[] empleados = new Empleado[10];
+
+    void agregarempleado(int pos)
+{ 
+    console.writeline("nombres")
+empleados[pos].nombre = console.readline();
+    Console.WriteLine("apellidos");
+    empleados[pos].apellido = console.readline();
+    Console.WriteLine("cargos");
+    empleados[pos].cargo = console.readline();
+    Console.WriteLine("salarios");
+    empleados[pos].salario = double.Parse(console.readline());
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("registro guardado sastisfactoriamente ");
+    Console.ResetColor();
+}
+
+
+
+
+struct Empleado
+{
+    public string nombre;
+    public string apellido;
+    public string cargo;
+    public double salario;
+
+}
